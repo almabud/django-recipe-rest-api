7a710d686a0bfb785469cb98fa24bf77dc5e5003
almabud <almabud37@gmail.com>
2020-01-13 03:01:39 +0600

    Added custom User Model

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
