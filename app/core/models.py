450ab5788ce3b43d8e052cf3ea5fc41f70cc43ec
almabud <almabud37@gmail.com>
2020-05-31 02:49:21 +0600

    Recipe end point added

--- app/core/models.py
+++ app/core/models.py
@@ -66,7 +66,7 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
-    ingredient = models.ManyToManyField('Ingredient')
+    ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
 
     def __str__(self):
